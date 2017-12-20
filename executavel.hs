c630d15b036a4c5ba22e229bd423de79b9d27a7a
AlbertoMGF <awefgaw@gmail.com>
2017-12-20 10:34:02 -0300

    Mudança de tipo no JSON
    Caso não saiba, esses testes são para este exercício da cadeira de Programação Funcional, específicamente(https://drive.google.com/file/d/0Bx3dnIGjvFl1ZUgtYWNlcm5SSTg/view).
    Veja a documentação do HUnit para Haskell sobre a execução dos testes de forma interativa.
    Compile o arquivo executavel.hs para exportar estatísticas sobre os testes via JSON.
    
    Novidades:
    
    - Mudança do tipo de dado no JSON: de array para object

--- executavel.hs
+++ executavel.hs
@@ -7,5 +7,6 @@ main = do
   tests <- (runTestTT mytests)
   let bd = words (showCounts tests)
   let success = read (bd !! 1) - read (bd !! 5) - read (bd !! 7)
-  let results = [matricula, read (bd !! 1), read (bd !! 5), read(bd !! 7), success]
-  writeFile "test-output.json" (encodeStrict (showJSONs results) ++ "\n\n%%%%%%%%%%%%%%%%%%%%%%%%%")
+  let list = [("matricula",matricula), ("totalTestes",read(bd !! 1)),("erros",read (bd !! 5)), ("falhas",read (bd !! 7)), ("passaram",success)]
+  let results = toJSObject list
+  writeFile "test-output.json" (encodeStrict results)
